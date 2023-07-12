32bde3633644ca40bde286749169c31c7c70717b
Jimmy Christensen <lithorus@gmail.com>
2023-07-12 23:03:17 +0200

    Fix getTools to new github structure

--- tools/getTools.sh
+++ tools/getTools.sh
@@ -7,17 +7,16 @@ source .env
 #curl -OL https://github.com/AcademySoftwareFoundation/OpenCue/releases/download/${OPENCUE_RELEASE}/cueadmin-${OPENCUE_RELEASE}-all.tar.gz
 #curl -OL https://github.com/AcademySoftwareFoundation/OpenCue/releases/download/${OPENCUE_RELEASE}/cuegui-${OPENCUE_RELEASE}-all.tar.gz
 
-virtualenv opencue-${OPENCUE_RELEASE} # If you previously created a virtualenv, skip this step.
-source opencue-${OPENCUE_RELEASE}/bin/activate
+virtualenv "opencue-${OPENCUE_RELEASE}" # If you previously created a virtualenv, skip this step.
+source "opencue-${OPENCUE_RELEASE}/bin/activate"
 
-for package in pycue pyoutline cueadmin cuegui cuesubmit rqd
+for PACKAGE in pycue pyoutline cueadmin cuegui cuesubmit rqd
 do
-    
-    curl -OL https://github.com/AcademySoftwareFoundation/OpenCue/releases/download/${OPENCUE_RELEASE}/$package-${OPENCUE_RELEASE}-all.tar.gz
-    export TAR=$package-${OPENCUE_RELEASE}-all.tar.gz
-    export DIR=$(basename "$TAR" .tar.gz)
-    tar xvzf "$TAR"
-    cd "$DIR"
+    curl -OL "https://github.com/AcademySoftwareFoundation/OpenCue/releases/download/v${OPENCUE_RELEASE}/${PACKAGE}-${OPENCUE_RELEASE}-all.tar.gz"
+    TAR=${PACKAGE}-${OPENCUE_RELEASE}-all.tar.gz
+    DIR=$(basename "${TAR}" .tar.gz)
+    tar xvzf "${TAR}"
+    cd "${DIR}"
     pip install -r requirements.txt
     if [[ -f requirements_gui.txt ]]
     then
@@ -25,5 +24,5 @@ do
     fi
     python setup.py install
     cd ..
-    rm -rf "$DIR"
+    rm -rf "${DIR}"
 done
