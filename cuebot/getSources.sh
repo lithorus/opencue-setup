1f8512babbd325b487184ccc359c151eec6159db
Jimmy Christensen <lithorus@gmail.com>
2023-07-12 22:38:55 +0200

    Fix download url for github

--- cuebot/getSources.sh
+++ cuebot/getSources.sh
@@ -3,5 +3,5 @@
 source .env
 mkdir deploy
 cd deploy
-curl -OL https://github.com/AcademySoftwareFoundation/OpenCue/archive/${OPENCUE_RELEASE}.tar.gz
-tar xvf ${OPENCUE_RELEASE}.tar.gz
+curl -o source.tar.gz -L https://github.com/AcademySoftwareFoundation/OpenCue/archive/refs/tags/v${OPENCUE_RELEASE}.tar.gz
+tar xvf source.tar.gz
